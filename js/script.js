d75c06454f88fa3cc1fe4187a6b55e6d5366af91
Umed <umedrah1mov@mail.ru>
2021-01-15 04:09:36 -0800

    ver: 0.0.1

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const lastMovie = prompt('Один из последних просмотренных фильмов?', '');
-
-const estimation = prompt('На сколько вы оцениваете его?', '');
-
-const personMovieDB = {
-    count: Number(numberOfFilms),
-    movies: {
-        lastMovie: estimation
-    },
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = +prompt('На сколько вы оцениваете его?', '');
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = +prompt('На сколько вы оцениваете его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
